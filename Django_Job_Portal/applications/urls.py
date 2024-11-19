92fa7f7b939d4641d4b5a5f3515b3524acbf3b77
Touhidul <toouhidul@gmail.com>
2024-11-19 16:38:20 +0600

    url is updated

--- Django_Job_Portal/applications/urls.py
+++ Django_Job_Portal/applications/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import CreateAndListApiview, JobApplicationStatusUpdateAPIView, AllJobApplicationDetailsView
 
 urlpatterns = [
-    path('application/<int:job_id>/', CreateAndListApiview.as_view(), name='application'),
-    path('application-user/<int:application_id>/',
+    path('<int:job_id>/', CreateAndListApiview.as_view(), name='application'),
+    path('status/<int:application_id>/',
          JobApplicationStatusUpdateAPIView.as_view(),
-         name='job-application'
+         name='job-application-status'
          ),
-    path('all/application/<int:job_id>/', AllJobApplicationDetailsView.as_view(), name='job-application-status'),
+    path('all/<int:job_id>/', AllJobApplicationDetailsView.as_view(), name='job-applications'),
 ]
